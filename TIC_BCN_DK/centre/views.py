4dee0282cbf3537dc8c912e14a5973a3022a82e5
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2024-03-13 19:00:52 +0100

    Fent el punt 2

--- TIC_BCN_DK/centre/views.py
+++ TIC_BCN_DK/centre/views.py
@@ -64,3 +64,31 @@ def students(request):
     ]
     context = {'alumne':alumnes}
     return render(request, 'alumne.html', context)
+def teacher(request, pk) {
+        professors = [
+        {    
+            "id": 1,
+            "nom":"Roger",
+            "cognom":"Sobrino",
+            "correu":"roger.sobrino@iticbcn.cat",
+            "curs":"DAM2B,DAW2A",
+            "moduls":"M7 Desenvolupament web d'entorn servidor"
+        },
+        {    
+            "id": 2,
+            "nom":"Josep Oriol",
+            "cognom":"Roca",
+            "correu":"oriol.roca@iticbcn.cat",
+            "curs":"DAM2B,DAW2A, DAW1A",
+            "moduls":"M9 Accessibilitat i usabilitat, M6 Desenvolupament web en entorn client"
+        },
+        {    
+            "id": 3,
+            "nom":"Juanma",
+            "cognom":"Biel",
+            "correu":"juanma.sanchez@iticbcn.cat",
+            "curs":"DAW2A",
+            "moduls":"M6 Desenvolupament web en entorn client"
+        },
+    ]
+}
