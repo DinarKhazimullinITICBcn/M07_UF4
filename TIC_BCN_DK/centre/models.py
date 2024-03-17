27d36ef95031824d336d13a6ebd605d01e0a832c
DinarKhazimullinITICBcn <146452935+DinarKhazimullinITICBcn@users.noreply.github.com>
2024-03-18 00:08:05 +0100

    Afegit tot, pero postgres no em deixa crear un server

--- TIC_BCN_DK/centre/models.py
+++ TIC_BCN_DK/centre/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nom = models.CharField(max_length=200)
+    cognom = models.CharField(max_length=200)
+    edat = models.IntegerField()
+    correu = models.CharField(max_length=200)
+    rol = models.CharField(max_length=200)
+    moduls = models.CharField(max_length=200)
